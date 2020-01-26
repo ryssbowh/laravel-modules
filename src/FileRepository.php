65d7e84c72d6dd5d4eb7b0a9a6c13f25665b5b8d
Boris Blondin <blondin.boris@gmail.com>
2020-01-26 07:48:42 +0000

    added getNonCoreModules method

--- src/FileRepository.php
+++ src/FileRepository.php
@@ -326,6 +326,28 @@ abstract class FileRepository implements RepositoryInterface, Countable
     }
 
     /**
+     * Get the non core modules
+     * 
+     * @param boolean $onlyNames
+     * 
+     * @return array
+     */
+    public function getNonCoreModules($onlyNames = false): array
+    {
+        $modules = array_filter($this->getOrdered(), function ($module) {
+            return $module->get('core') != 1;
+        });
+
+        if ($onlyNames) {
+            return array_map(function ($module) {
+                return $module->getName();
+            }, $modules);
+        }
+
+        return $modules;
+    }
+
+    /**
      * Get a module path.
      *
      * @return string
